062b24bc5981aab2e6ac17fcb874cf776741530e
28331 Banco ABC <28331bancoabc@MacBook-Pro-de-28331.local>
2020-10-26 07:51:35 -0300

    style errros

---
+++ Untitled-1.js
@@ -0,0 +1,29 @@
+  // container: {
+  //   // flex: 1,
+  //   justifyContent: 'flex-start',
+  //   alignItems: 'center',
+  //   marginTop: 35,
+  //   backgroundColor: '#ffffff',
+  // },
+  // viewText: {
+  //   marginTop: 50,
+  //   width: '50%',
+  //   alignItems: 'center',
+
+  //   backgroundColor: 'red',
+  // },
+  // pdf: {
+  //   flex: 1,
+
+  //   width: Dimensions.get('window').width,
+  //   height: height * 0.7,
+  // },
+  // scroolView: {
+  //   // flex: 1,
+  //   backgroundColor: 'white',
+  //   height: height * 0.7,
+
+  //   // height: height * 0.1,
+
+  //   // height: Dimensions.get('window').height,
+  // },
